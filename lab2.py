d5378f6b441e7865692761fe4c648e3dd5093dee
danieleyton <daniel.labra@usach.cl>
2020-08-16 16:59:41 -0400

    algunos arreglos al menu

--- lab2.py
+++ lab2.py
@@ -3,8 +3,16 @@ import sim
 import sys
 import numpy as np
 
-def automaticDrive(clientID, leftSensor, middleSensor, rightSensor, leftJointDynamic, rightJointDynamic, wheelRadius, nominalLinearVelocity):
-    while(True):
+def automaticDrive(stdscr):
+    stdscr.clear()
+    stdscr.nodelay(1)
+    stdscr.border(0)
+    stdscr.addstr(2, 10, "En modo Automatico...")
+    stdscr.addstr(4, 10, "Presione 'q' para salir")
+    stdscr.refresh()
+
+    key = ''
+    while key != ord('q'):
         # read the sensors:
         sensorReading=[False, False, False]
         sensorReading[0]=(sim.simxReadVisionSensor(clientID, leftSensor, sim.simx_opmode_blocking)==1)
@@ -17,7 +25,7 @@ def automaticDrive(clientID, leftSensor, middleSensor, rightSensor, leftJointDyn
         # decide about left and right velocities:
         linearVelocityLeft=nominalLinearVelocity
         linearVelocityRight=nominalLinearVelocity
-        print(dataLeft[0][10])
+        #print(dataLeft[0][10])
         if(dataLeft[0][10]>0.7 and dataRight[0][10]<=0.2):
             linearVelocityRight=linearVelocityRight*0.4
             #linearVelocityLeft=nominalLinearVelocity*0.7
@@ -32,8 +40,16 @@ def automaticDrive(clientID, leftSensor, middleSensor, rightSensor, leftJointDyn
         sim.simxSetJointTargetVelocity(clientID, leftJointDynamic, linearVelocityLeft/wheelRadius, sim.simx_opmode_blocking)
         sim.simxSetJointTargetVelocity(clientID, rightJointDynamic, linearVelocityRight/wheelRadius, sim.simx_opmode_blocking)
 
+        key = stdscr.getch()
+    stdscr.clear()
+    curses.endwin()
+
 def manualControl(stdscr):
     # nu se unu
+    #stdscr = curses.newwin(40, 40, 4, 7)
+    stdscr.clear()
+    stdscr.nodelay(0)
+    stdscr.border(0)
     stdscr.addstr(0,10,"Presione 'q' para salir")
     stdscr.refresh()
 
@@ -69,19 +85,20 @@ def manualControl(stdscr):
             linearVelocityRight = 0.3*0.3
             sim.simxSetJointTargetVelocity(clientID, leftJointDynamic, linearVelocityLeft/wheelRadius, sim.simx_opmode_blocking)
             sim.simxSetJointTargetVelocity(clientID, rightJointDynamic, linearVelocityRight/wheelRadius, sim.simx_opmode_blocking)
+    stdscr.clear()
     curses.endwin()
 
-def main(stdscr):
-    #stdscr = curses.initscr()
+def main(stdscr): 
     curses.noecho()
     curses.cbreak()
     stdscr.keypad(True)
+    stdscr.border(0)
     sim.simxFinish(-1) # Terminar todas las conexiones
     global clientID, lineTracerBase, leftSensor, middleSensor, rightSensor, leftJoin, rightJoint, leftJointDynamic, rightJointDynamic, nominalLinearVelocity, wheelRadius, interWheelDistance
     clientID=sim.simxStart('127.0.0.1',19999,True,True,5000,5) # Iniciar una nueva conexion
 
     if clientID != -1:
-        print("Conexion establecida")
+        stdscr.addstr(4, 10, "Conexion establecida")
         res, lineTracerBase = sim.simxGetObjectHandle(clientID, 'LineTracerBase', sim.simx_opmode_blocking)
         res, leftSensor = sim.simxGetObjectHandle(clientID, 'LeftSensor', sim.simx_opmode_blocking)
         res, middleSensor = sim.simxGetObjectHandle(clientID, 'MiddleSensor', sim.simx_opmode_blocking)
@@ -90,7 +107,7 @@ def main(stdscr):
         res, rightJoint= sim.simxGetObjectHandle(clientID, 'RightJoint', sim.simx_opmode_blocking)
         res, leftJointDynamic= sim.simxGetObjectHandle(clientID, 'DynamicLeftJoint', sim.simx_opmode_blocking)
         res, rightJointDynamic= sim.simxGetObjectHandle(clientID, 'DynamicRightJoint', sim.simx_opmode_blocking)
-        nominalLinearVelocity = 0.08
+        nominalLinearVelocity = 0.09
         wheelRadius = 0.027
         interWheelDistance = 0.119
 
@@ -107,14 +124,18 @@ def main(stdscr):
         sim.simxSetJointTargetVelocity(clientID, rightJointDynamic, 0/wheelRadius, sim.simx_opmode_blocking)
 
         opt = 0
+        stdscr.clear()
+        stdscr.border()
         while opt != ord('3'):
-            stdscr.addstr(0, 10, "Para usar el modo automatico presione 1\nPara usar el modo manual presione 2\nPara salir presione 3")
+            stdscr.addstr(4, 10, "Para usar el modo automatico presione 1")
+            stdscr.addstr(6, 10, "Para usar el modo manual presione 2")
+            stdscr.addstr(8, 10, "Para salir presione 3")
             opt = stdscr.getch()
 
             if opt == ord('1'):
                 # enter modo automatico
                 stdscr.addstr(2,10, "Enter modo automatico")
-                automaticDrive(clientID, leftSensor, middleSensor, rightSensor, leftJointDynamic, rightJointDynamic, wheelRadius, nominalLinearVelocity)
+                automaticDrive(stdscr)
             elif opt == ord('2'):
                 # enter modo manual
                 stdscr.addstr(4, 10, "Enter modo manual")
@@ -128,7 +149,7 @@ def main(stdscr):
 
         
     else:
-        print("Error: no se puede conectar")
+        stdscr.addstr(4, 10, "Error: no se puede conectar")
         sys.exit("Error: no se puede conectar")
 
 curses.wrapper(main)
